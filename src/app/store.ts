bed6d3e90e2a0a6a6c721eb5eb18b020187aeeb2
Timmy Luong <TCL735@users.noreply.github.com>
2024-07-02 10:36:32 -0700

    chore: remove unused boilerplate example

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import { birthsReducer } from "../features/onThisDay/birthsSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     births: birthsReducer,
   },
 });
